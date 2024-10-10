1b83287df6ee365664cc9f67268c8cbd4f06d9bc
Sead Feng <seadfeng@gmail.com>
2024-10-10 18:43:31 +0800

    fix Access

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,11 +1,11 @@
- 
+
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   // Create a new response with updated headers
   const response = NextResponse.next();
   const apikey = req.headers.get("x-api-key");
-  if(apikey !== process.env.NEXT_API_KEY) new NextResponse('Access Denied', { status: 403 });
+  if (apikey !== process.env.NEXT_API_KEY) return new NextResponse('Access Denied', { status: 403 });
   response.headers.set('x-request-url', req.url);
 
   return response;
