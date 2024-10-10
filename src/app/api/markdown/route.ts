cfa85feda2daaca510132632a3a22841bca10314
Sead Feng <seadfeng@gmail.com>
2024-10-10 18:29:54 +0800

    update api/markdown

--- src/app/api/markdown/route.ts
+++ src/app/api/markdown/route.ts
@@ -2,6 +2,12 @@ import { marked } from 'marked';
 import type { NextRequest } from 'next/server';
 export const runtime = 'edge'
 
+
+/**
+ * Markdown to HTML with Marked 
+ * 
+ * @returns 
+ */
 export async function POST(request: NextRequest) {
   const { content } = await request.json() as {
     content: string;
